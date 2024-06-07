db0ba5110fa01dc0f98a59d066246d3276c5399c
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-06-07 15:01:05 -0400

    added logout button

--- src/components/HomeNavbar/HomeNavbar.tsx
+++ src/components/HomeNavbar/HomeNavbar.tsx
@@ -1,7 +1,9 @@
+import { usePrivy } from '@privy-io/react-auth';
+import { useState, MouseEvent, KeyboardEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Paper, styled } from '@mui/material';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+import { Paper, styled, Box, IconButton } from '@mui/material';
+import { SwipeableDrawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 import HomeIcon from '@mui/icons-material/Home';
 import AddIcon from '@mui/icons-material/Add';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -13,9 +15,58 @@ const Navigation = styled(Box)(({ theme }) => ({
     backgroundColor: '#fff',
 }));
 
-// TODO: Fix styling on large screens, navbar extends further than the apps page
+const SettingsDrawer = ({ isOpen, toggleDrawer }: { isOpen: boolean, toggleDrawer: (open: boolean) => (event: KeyboardEvent | MouseEvent) => void }) => {
+    const navigate = useNavigate();
+    const {ready, authenticated, logout} = usePrivy();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/');
+    };
+
+    const drawerList = () => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                <ListItem key="logout" disablePadding>
+                    <ListItemButton disabled={!ready || (ready && !authenticated)} onClick={handleLogout}>
+                        <ListItemIcon>
+                            <LogoutIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Logout" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Divider />
+        </Box>
+    );
+
+    return (
+        <SwipeableDrawer
+            anchor="bottom"
+            open={isOpen}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+        >
+            {drawerList()}
+        </SwipeableDrawer>
+    );
+};
+
 function HomeNavbar() {
     const navigate = useNavigate();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = (open: boolean) => (event: KeyboardEvent | MouseEvent) => {
+        if (event && event.type === 'keydown' && ((event as KeyboardEvent).key === 'Tab' || (event as KeyboardEvent).key === 'Shift')) {
+            return;
+        }
+        setDrawerOpen(open);
+    };
 
     return (
         <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
@@ -23,16 +74,16 @@ function HomeNavbar() {
                 <IconButton onClick={() => navigate('/home')}>
                     <HomeIcon />
                 </IconButton>
-                {/* TODO: Possibly highlight, tooltip, if user has no account */}
                 <IconButton onClick={() => navigate('/create')}>
                     <AddIcon />
                 </IconButton>
-                <IconButton onClick={() => navigate('/')}>
+                <IconButton onClick={toggleDrawer(true)}>
                     <MenuIcon />
                 </IconButton>
             </Navigation>
+            <SettingsDrawer isOpen={drawerOpen} toggleDrawer={toggleDrawer} />
         </Paper>
-    )
+    );
 }
 
-export default HomeNavbar
+export default HomeNavbar;
