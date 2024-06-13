f24a07630942075752ad909edc9d240a0f3ecb8d
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-06-12 22:54:22 -0400

    wip: implement contracts

--- memebank-contracts/src/Account.sol
+++ memebank-contracts/src/Account.sol
@@ -1,44 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@synthetixio/core-modules/contracts/interfaces/IAccountProxy.sol";
-import "@synthetixio/core-modules/contracts/interfaces/IEngine.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPerpsMarketProxy} from "./external/IPerpsMarketProxy.sol";
+import {IEngine} from "./external/IEngine.sol";
 
 /// @title Account
 /// @notice Contract representing a user's trading account on Kwenta
-contract Account {
-    address public owner;
-    address public kwentaProxy;
-    address public kwentaEngine;
-    address public collateralToken;
+contract Account is Ownable {
+    IPerpsMarketProxy public perpsMarketProxy;
+    IEngine public engine;
+    IERC20 public sUSD;
+    IERC20 public USDC;
+
+    // ACTOR's account id in the Synthetix v3 perps market
+    uint128 public accountId;
 
     /// @notice Emitted when collateral is deposited
-    event CollateralDeposited(address indexed token, uint256 amount);
+    event CollateralDeposited(address indexed token, int256 amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
-    }
-
-    constructor(address _owner, address _kwentaProxy, address _kwentaEngine, address _collateralToken) {
-        owner = _owner;
-        kwentaProxy = _kwentaProxy;
-        kwentaEngine = _kwentaEngine;
-        collateralToken = _collateralToken;
+    constructor(
+        address _perpsMarketProxyAddress,
+        address _engineAddress,
+        address _sUSDAddress,
+        address _usdcAddress
+    ) Ownable(msg.sender) {
+        perpsMarketProxy = IPerpsMarketProxy(_perpsMarketProxyAddress);
+        engine = IEngine(_engineAddress);
+        sUSD = IERC20(_sUSDAddress);
+        USDC = IERC20(_usdcAddress);
 
         // Create account in Kwenta and grant permissions
-        IAccountProxy(kwentaProxy).createAccount();
-        IAccountProxy(kwentaProxy).grantPermission(address(this), kwentaEngine);
+        accountId = perpsMarketProxy.createAccount();
+        perpsMarketProxy.grantPermission({
+            accountId: accountId,
+            permission: "ADMIN",
+            user: address(engine)
+        });
     }
 
     /// @notice Function to approve and deposit collateral
     /// @param amount The amount of collateral to deposit
-    function depositCollateral(uint256 amount) external onlyOwner {
-        IERC20(collateralToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(collateralToken).approve(kwentaEngine, amount);
-        IEngine(kwentaEngine).modifyCollateral(address(this), collateralToken, int256(amount));
-        emit CollateralDeposited(collateralToken, amount);
+    function depositCollateral(int256 amount) external onlyOwner {
+        USDC.approve(address(engine), uint256(amount));
+        
+        engine.modifyCollateralZap({
+            _accountId: accountId,
+            _amount: amount
+        });
+        
+        emit CollateralDeposited(address(USDC), amount);
     }
 
     /// @notice Function to execute a trade on Kwenta
--- memebank-contracts/src/AccountFactory.sol
+++ memebank-contracts/src/AccountFactory.sol
@@ -7,23 +7,36 @@ import "./Account.sol";
 /// @title AccountFactory
 /// @notice Factory contract to create and manage accounts for Kwenta trading
 contract AccountFactory is Ownable {
-    address public kwentaProxy;
-    address public kwentaEngine;
-    address public collateralToken;
+    address public perpsMarketProxy;
+    address public engine;
+    address public sUSD;
+    address public USDC;
 
     /// @notice Emitted when a new account is created
     event AccountCreated(address indexed account, address indexed creator);
 
-    constructor(address _kwentaProxy, address _kwentaEngine, address _collateralToken) {
-        kwentaProxy = _kwentaProxy;
-        kwentaEngine = _kwentaEngine;
-        collateralToken = _collateralToken;
+    constructor(
+        address _perpsMarketProxy,
+        address _engine,
+        address _sUSD,
+        address _USDC
+    ) Ownable(msg.sender) {
+        perpsMarketProxy = _perpsMarketProxy;
+        engine = _engine;
+        sUSD = _sUSD;
+        USDC = _USDC;
     }
 
     /// @notice Function to create a new account
     function createAccount() external returns (address) {
-        Account newAccount = new Account(msg.sender, kwentaProxy, kwentaEngine, collateralToken);
+        Account newAccount = new Account(
+            perpsMarketProxy,
+            engine,
+            sUSD,
+            USDC
+        );
         emit AccountCreated(address(newAccount), msg.sender);
+        newAccount.transferOwnership(msg.sender); // Transfer ownership to the account creator
         return address(newAccount);
     }
 }
