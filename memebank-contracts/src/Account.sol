b625bd84de137c3870b9af73c4ff2bae3108db81
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-06-13 13:00:44 -0400

    wip: base contracts

--- memebank-contracts/src/Account.sol
+++ memebank-contracts/src/Account.sol
@@ -5,6 +5,8 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPerpsMarketProxy} from "./external/IPerpsMarketProxy.sol";
 import {IEngine} from "./external/IEngine.sol";
+import {ISynthetixCore} from "./external/ISynthetixCore.sol";
+import {AccountFactory} from "./AccountFactory.sol";
 
 /// @title Account
 /// @notice Contract representing a user's trading account on Kwenta
@@ -13,6 +15,7 @@ contract Account is Ownable {
     IEngine public engine;
     IERC20 public sUSD;
     IERC20 public USDC;
+    AccountFactory public accountFactory;
 
     // ACTOR's account id in the Synthetix v3 perps market
     uint128 public accountId;
@@ -20,16 +23,39 @@ contract Account is Ownable {
     /// @notice Emitted when collateral is deposited
     event CollateralDeposited(address indexed token, int256 amount);
 
+    /// @notice Emitted when contract addresses are updated
+    event AddressesUpdated(
+        IPerpsMarketProxy perpsMarketProxy,
+        IEngine engine,
+        IERC20 sUSD,
+        IERC20 USDC
+    );
+
+    /// @notice Emitted when an order is committed
+    event OrderCommitted(
+        IPerpsMarketProxy.Data retOrder,
+        uint128 perpsMarketId,
+        uint128 accountId,
+        int128 sizeDelta,
+        uint128 settlementStrategyId,
+        uint256 acceptablePrice,
+        bytes32 trackingCode,
+        address referrer,
+        uint256 fees
+    );
+
     constructor(
-        address _perpsMarketProxyAddress,
-        address _engineAddress,
-        address _sUSDAddress,
-        address _usdcAddress
+        IPerpsMarketProxy _perpsMarketProxy,
+        IEngine _engine,
+        IERC20 _sUSD,
+        IERC20 _usdc,
+        AccountFactory _accountFactory
     ) Ownable(msg.sender) {
-        perpsMarketProxy = IPerpsMarketProxy(_perpsMarketProxyAddress);
-        engine = IEngine(_engineAddress);
-        sUSD = IERC20(_sUSDAddress);
-        USDC = IERC20(_usdcAddress);
+        perpsMarketProxy = _perpsMarketProxy;
+        engine = _engine;
+        sUSD = _sUSD;
+        USDC = _usdc;
+        accountFactory = _accountFactory;
 
         // Create account in Kwenta and grant permissions
         accountId = perpsMarketProxy.createAccount();
@@ -38,25 +64,80 @@ contract Account is Ownable {
             permission: "ADMIN",
             user: address(engine)
         });
+
+        // Grant additional permissions to AccountFactory
+        // TODO: Only enable for
+        perpsMarketProxy.grantPermission({
+            accountId: accountId,
+            permission: "ADMIN",
+            user: address(accountFactory)
+        });
     }
 
     /// @notice Function to approve and deposit collateral
     /// @param amount The amount of collateral to deposit
     function depositCollateral(int256 amount) external onlyOwner {
-        USDC.approve(address(engine), uint256(amount));
-        
-        engine.modifyCollateralZap({
-            _accountId: accountId,
-            _amount: amount
-        });
-        
         emit CollateralDeposited(address(USDC), amount);
+        
+        bool success = USDC.approve(address(engine), uint256(amount));
+        require(success, "USDC approval failed");
+
+        engine.modifyCollateralZap({_accountId: accountId, _amount: amount});
     }
 
     /// @notice Function to execute a trade on Kwenta
-    /// @param market The market to trade on
-    /// @param amount The amount to trade
-    function executeTrade(address market, uint256 amount) external onlyOwner {
-        // Implement trade logic here using Kwenta's Engine
+    /// @param perpsMarketId The ID of the perpetual market
+    /// @param sizeDelta The size delta for the trade
+    /// @param settlementStrategyId The ID of the settlement strategy
+    /// @param acceptablePrice The acceptable price for the trade
+    /// @param trackingCode The tracking code for the trade
+    /// @param referrer The address of the referrer
+    function executeTrade(
+        uint128 perpsMarketId,
+        int128 sizeDelta,
+        uint128 settlementStrategyId,
+        uint256 acceptablePrice,
+        bytes32 trackingCode,
+        address referrer
+    ) external onlyOwner {
+        (IPerpsMarketProxy.Data memory retOrder, uint256 fees) = engine
+            .commitOrder({
+                _perpsMarketId: perpsMarketId,
+                _accountId: accountId,
+                _sizeDelta: sizeDelta,
+                _settlementStrategyId: settlementStrategyId,
+                _acceptablePrice: acceptablePrice,
+                _trackingCode: trackingCode,
+                _referrer: referrer
+            });
+        // @note Necessary to emit event after external call as we don't have fee/retOrder data
+        //slither-disable-next-line low-level-calls
+        emit OrderCommitted(
+            retOrder,
+            perpsMarketId,
+            accountId,
+            sizeDelta,
+            settlementStrategyId,
+            acceptablePrice,
+            trackingCode,
+            referrer,
+            fees
+        );
+    }
+
+    /// @notice Updates the addresses of perpsMarketProxy, engine, sUSD, and USDC from the AccountFactory
+    function updateAddresses() external onlyOwner {
+        IPerpsMarketProxy newPerpsMarketProxy = accountFactory
+            .perpsMarketProxy();
+        IEngine newEngine = accountFactory.engine();
+        IERC20 newSUSD = accountFactory.sUSD();
+        IERC20 newUSDC = accountFactory.USDC();
+
+        perpsMarketProxy = newPerpsMarketProxy;
+        engine = newEngine;
+        sUSD = newSUSD;
+        USDC = newUSDC;
+
+        emit AddressesUpdated(perpsMarketProxy, engine, sUSD, USDC);
     }
 }
--- memebank-contracts/src/AccountFactory.sol
+++ memebank-contracts/src/AccountFactory.sol
@@ -1,42 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPerpsMarketProxy} from "./external/IPerpsMarketProxy.sol";
+import {IEngine} from "./external/IEngine.sol";
 import "./Account.sol";
 
 /// @title AccountFactory
 /// @notice Factory contract to create and manage accounts for Kwenta trading
 contract AccountFactory is Ownable {
-    address public perpsMarketProxy;
-    address public engine;
-    address public sUSD;
-    address public USDC;
+    IPerpsMarketProxy public perpsMarketProxy;
+    IEngine public engine;
+    IERC20 public sUSD;
+    IERC20 public USDC;
 
     /// @notice Emitted when a new account is created
     event AccountCreated(address indexed account, address indexed creator);
+    
+    /// @notice Emitted when contract addresses are updated
+    event AddressesUpdated(
+        IPerpsMarketProxy perpsMarketProxy,
+        IEngine engine,
+        IERC20 sUSD,
+        IERC20 USDC
+    );
 
     constructor(
-        address _perpsMarketProxy,
-        address _engine,
-        address _sUSD,
-        address _USDC
+        address _perpsMarketProxyAddress,
+        address _engineAddress,
+        address _sUSDAddress,
+        address _usdcAddress
     ) Ownable(msg.sender) {
-        perpsMarketProxy = _perpsMarketProxy;
-        engine = _engine;
-        sUSD = _sUSD;
-        USDC = _USDC;
+        perpsMarketProxy = IPerpsMarketProxy(_perpsMarketProxyAddress);
+        engine = IEngine(_engineAddress);
+        sUSD = IERC20(_sUSDAddress);
+        USDC = IERC20(_usdcAddress);
     }
 
     /// @notice Function to create a new account
+    /// @return address The address of the newly created account
     function createAccount() external returns (address) {
-        Account newAccount = new Account(
-            perpsMarketProxy,
-            engine,
-            sUSD,
-            USDC
-        );
-        emit AccountCreated(address(newAccount), msg.sender);
-        newAccount.transferOwnership(msg.sender); // Transfer ownership to the account creator
+        Account newAccount = new Account({
+            _perpsMarketProxy: perpsMarketProxy,
+            _engine: engine,
+            _sUSD: sUSD,
+            _usdc: USDC,
+            _accountFactory: AccountFactory(address(this))
+        });
+        // Transfer ownership to the account creator
+        newAccount.transferOwnership(msg.sender);
         return address(newAccount);
+    }
+
+    /// @notice Function to update the addresses of perpsMarketProxy, engine, sUSD, and USDC
+    /// @param newPerpsMarketProxy The new address of the PerpsMarketProxy
+    /// @param newEngine The new address of the Engine
+    /// @param newSUSD The new address of the sUSD token
+    /// @param newUSDC The new address of the USDC token
+    function updateAddresses(
+        address newPerpsMarketProxy,
+        address newEngine,
+        address newSUSD,
+        address newUSDC
+    ) external onlyOwner {
+        perpsMarketProxy = IPerpsMarketProxy(newPerpsMarketProxy);
+        engine = IEngine(newEngine);
+        sUSD = IERC20(newSUSD);
+        USDC = IERC20(newUSDC);
+
+        emit AddressesUpdated(IPerpsMarketProxy(newPerpsMarketProxy), IEngine(newEngine), IERC20(newSUSD), IERC20(newUSDC));
     }
 }
