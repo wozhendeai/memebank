bf4bb39a25f0174914c4dba25fe3c2219e5a697a
cjc02 <126291407+cjc02@users.noreply.github.com>
2024-06-23 14:31:01 -0400

    update deployment args and script

--- memebank-hardhat/ignition/modules/AccountFactory.t.ts
+++ memebank-hardhat/ignition/modules/AccountFactory.t.ts
@@ -1,10 +1,5 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-// Mainnet addresses TODO: import from mainnet?
-export const PERPS_MARKET_PROXY_ADDRESS = "0x0A2AF931eFFd34b81ebcc57E3d3c9B1E1dE1C9Ce";
-export const ENGINE_ADDRESS = "0xe331a7eeC851Ba702aA8BF43070a178451d6D28E";
-export const SUSD_ADDRESS = "0xC74eA762cF06c9151cE074E6a569a5945b6302E7";
-export const USDC_ADDRESS = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"; // deprecated
+import { PERPS_MARKET_PROXY_ADDRESS, ENGINE_ADDRESS, SUSD_ADDRESS, USDC_ADDRESS, SNX_ADDRESS } from "../../test/utils/Constants";
 
 export default buildModule("DeployAccountFactory", (m) => {
 
@@ -13,6 +8,7 @@ export default buildModule("DeployAccountFactory", (m) => {
         ENGINE_ADDRESS,
         SUSD_ADDRESS,
         USDC_ADDRESS,
+        SNX_ADDRESS
     ]);
 
 
